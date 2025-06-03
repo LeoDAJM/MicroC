4dc6eb9559c1528c560fe432f4da4d2371ea0041
Ale_Linux <diego.jimenez.m.2001@gmail.com>
2025-06-03 12:25:24 -0400

    UBC finalizada, ALU Banderas Pendientes

--- MicroC.h
+++ MicroC.h
@@ -3,12 +3,14 @@
 
 #pragma once
 
+#include <bit>
+#include <bitset>
+#include <cstdint>
 #include <iostream>
 
 #ifndef RAM64K_HPP
 #define RAM64K_HPP
 
-#include <cstdint>
 #include <cstring> // Para std::memset
 
 class RAM64K {
@@ -53,49 +55,87 @@ public:
 
 #endif // RAM64K_HPP
 
-class UBC {
-private:
-    static void execute(uint8_t& X, uint8_t& Y, uint8_t& cw, uint8_t& S, uint8_t& A, uint8_t& B) {
-        A = (X & -((cw >> 4) & 1)) ^ -((cw >> 2) & 1);
-        B = (Y & -((cw >> 3) & 1)) ^ -((cw >> 1) & 1);
-		adder8(A, B, S, cw);
+inline void adder8(uint8_t& A, uint8_t& B, uint8_t& result, uint8_t& control, uint8_t& batch) {
+    // Control Word (Co, CW[6], Cin)
+    // x x MuxCin AND(2) XOR(2) Cin
+    // batch1
+    // x x x x x x Co Cin2
+    uint8_t G = {0}; // Generate
+    uint8_t P = {0}; // Propagate
+    uint8_t C = (control & ~(control >> 5)) | (batch & (control >> 5)) & 1; // Carries
+    P = A ^ B;
+    G = A & B;
+    // Calculamos acarreos usando carry lookahead
+    for (int i = 1; i <= 7; ++i) {
+        // C[i] = G[i-1] | (P[i-1] & C[i-1])
+        C |= ((G | (P & C)) & (1 << (i - 1))) << 1;
     }
-public:
-    static void adder8(uint8_t& A, uint8_t& B, uint8_t& result, uint8_t& control) {
-        // Control Word (Co, CW[6], Cin)
-        uint8_t G = {0}; // Generate
-        uint8_t P = {0}; // Propagate
-        uint8_t C = control; // Carries
+    batch |= ((G | (P & C)) & 0x80) >> 6;
+    result = P ^ C;
+}
 
-        P = A ^ B;
-        G = A & B;
-        C &= 1;
-        // Calculamos acarreos usando carry lookahead
-        for (int i = 1; i <= 7; ++i) {
-            // C[i] = G[i-1] | (P[i-1] & C[i-1])
-            C |= ((G | (P & C)) & (1 << (i - 1))) << 1;
-        }
-        control |= ((G | (P & C)) & 0x80);
-        result = P ^ C;
+inline void UBC(uint8_t& X, uint8_t& Y, uint8_t& cw, uint8_t& batch, uint8_t& S) {
+    uint8_t A = (X & -((cw >> 4) & 1)) ^ -((cw >> 2) & 1);
+    uint8_t B = (Y & -((cw >> 3) & 1)) ^ -((cw >> 1) & 1);
+	adder8(A, B, S, cw, batch);
+}
+
+// Barrel Shifter
+inline void BS(uint8_t& A, uint16_t& cw, uint8_t& batch, uint8_t& result) {
+    /* 
+    0 0 0 -> Rot. Der. woC
+    0 0 1 -> Rot. Der. wC
+    0 1 0 -> Rot. Izq. woC
+    0 1 1 -> Rot. Izq. wC
+    1 0 0 -> Des. Der. Arith.
+    1 0 1 -> Des. Der. Log.
+    1 1 0 -> Des. Izq. Arith.
+    1 1 1 -> Des. Izq. Log.
+    batch1
+    x x x x x x CF Cin2
+    */
+    switch (cw & 0x7)
+    {
+    case 0:
+        result = std::rotr(A, 1);
+        break;
+    case 1:
+        result = A >> 1;        //ResW
+        result |= (batch << 6) & 0x80;  //MSBRes
+        batch &= 0xFD;
+        batch |= (A & 1) << 1;  //CF
+        break;
+    case 2:
+        result = std::rotl(A, 1);
+        break;
+    case 3:
+        result = A << 1;        //ResW
+        result |= (batch >> 1) & 0x1;  //LSBRes
+        batch &= 0xFD;
+        batch |= (A & 0x80) >> 6;  //CF
+        break;
+    case 4:
+        result = (A >> 1) | (A & 0x80);
+        break;
+    case 5:
+        result = A >> 1;
+        break;
+    case 6:
+        result = (A << 1) | (A & 0x80);
+        break;
+    case 7:
+        result = A << 1;
+        break;
+    default:
+        std::cout << "Error S[MUX TD]" << std::endl;
+        break;
     }
-};
 
-class UBC {
-private:
-    uint8_t X = { 0 };  // Entrada A
-    uint8_t Y = { 0 };  // Entrada B
-    uint8_t cw = { 0 };  // Control Word (Co, MUX (S6-5), CW[4] (S4-1), Cin (S0) )
-    uint8_t S = { 0 };  // Suma S
-	ubc = UBC();
-public:
+}
 
-    void mux(uint8_t A, uint8_t B, uint8_t* result, uint8_t* control) {
-        // Control Word (Co, CW[6], Cin)
-        uint8_t R = { 0 }; // Generate
-
-		uint8_t muxS = (*control >> 5) & 0x03; // MUX selector bits
-
-        switch (muxS) {
+inline void ALU (uint8_t& A, uint8_t& B, uint16_t& cw, uint8_t& batch, uint8_t& R) {
+    uint8_t tmp;
+    switch ((cw >> 6) & 0x7) {
         case 0:
             R = A & B;
             break;
@@ -106,18 +146,49 @@ public:
             R = A ^ B;
             break;
         case 3:
+            // uint8_t y = static_cast<uint8_t>(x);
+            tmp = cw;
+            UBC(A, B, tmp, batch, R);
+            break;
+        case 4:
+            if ((cw >> 3) & 1) tmp = B;
+            else tmp = A;
             
-        }
+            BS(tmp, cw, batch, R);
+            break;
+        default:
+            std::cout << "Error S[MUX ALU]" << std::endl;
+            break;
+    }
+    //    batch1
+    //      x HF PF ZF NF VF CF Cin2
+    for (uint_fast8_t i = 0; i < 8; i++)
+    {
+        batch |= ((R >> i) & 0x1) << 4; //ZF
+    }
+    batch |= (R >> 4) & 0x08; //NF Signo
 
-        P = A ^ B;
-        G = A & B;
-        C &= 1;
-        // Calculamos acarreos usando carry lookahead
-        for (int i = 1; i <= 7; ++i) {
-            // C[i] = G[i-1] | (P[i-1] & C[i-1])
-            C |= ((G | (P & C)) & (1 << (i - 1))) << 1;
-        }
-        *control |= ((G | (P & C)) & 0x80);
-        *result = P ^ C;
+    
+    Z = 
+    batch |= ((cw >> 3) & (cw >> 3) & cw)
+}
+
+class USCS
+{
+private:
+    uint8_t A = { 0 };
+    uint8_t B = { 0 };
+    uint32_t cw = { 0 };
+    uint8_t R = { 0 };
+    uint8_t F = { 0 };
+public:
+    inline void run () {
+        ALU(A, B, cw, F, R);
+        R = B;
     }
 };
+
+
+inline void USCS (uint8_t& A, uint8_t& cw, uint8_t& F, uint8_t& R) {
+
+}
