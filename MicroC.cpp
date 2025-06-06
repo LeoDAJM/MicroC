f63d3a4a5a4383dea83d25ff1aa575096e7af7ae
LeoDAJM <diego.jimenez.m.2001@gmail.com>
2025-06-06 18:10:40 -0400

    Interfaz sencilla para probar USCS

--- MicroC.cpp
+++ MicroC.cpp
@@ -10,16 +10,19 @@ int main()
 	cout << "Hello CMake." << endl;
 	ModUSCS USCSimple;
 	string s;
+	int tmp;
 	while (true) {
 		cout << "Señal[S] : ";
 		cin >> s; // Suppose s = "100100101"
+		cout << "|" << s << "|" << endl;
 		USCSimple.cw = (uint32_t)bitset<32>(s).to_ulong();
 		if (USCSimple.cw == 0) break; // Exit condition
 		cout << endl;
 		cout << "Valor B : ";
-		cin >> USCSimple.B; // Suppose s = "100100101"
+		cin >> tmp; // Suppose s = "100100101"
+		USCSimple.B = static_cast<uint8_t>(tmp);
 		cout << endl << "----------------------------------" << endl;
-		cout << hex << "B IN: " << USCSimple.B << "Flags: " << USCSimple.F << "R: " << USCSimple.R << endl;
+		cout << hex << "B IN: " << static_cast<int>(USCSimple.B) << "		Flags: " << static_cast<int>(USCSimple.F) << "R: " << static_cast<int>(USCSimple.R) << endl;
 		cout << "----------------------------------" << endl;
 	}
 	return 0;
